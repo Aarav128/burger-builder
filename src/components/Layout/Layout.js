71a8dfffcb2f62bd59bd0d6aa401601ddc0a60b2
Aarav Shah <aaravinaction@gmail.com>
2020-04-14 11:21:03 -0700

    Set up base burger builder container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import classes from './Layout.css'
+
 import Aux from '../../hoc/Aux'
 
 const layout = ({ children }) => (
     <Aux>
         <div>Toolbar, SideDrawer, BackDrop</div>
-        <main>
+        <main className={classes.Content}>
             {children}
         </main>
     </Aux>
