3603fd75d32b6df458163d8b8812fa289374e68f
Aarav Shah <aaravinaction@gmail.com>
2020-04-14 17:48:45 -0700

    render burger ingredients based on object

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,19 @@ import classes from './Burger.css'
 
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const burger = props =>
+const burger = ({ ingredients }) =>
 {
+    const igList = Object.keys(ingredients)
+        .map(igKey => 
+        {
+            return [...Array(ingredients[igKey])].map((_, i) => 
+            {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
+        })
     return <div className={classes.Burger}>
         <BurgerIngredient type='bread-top' />
-        <BurgerIngredient type='cheese' />
-        <BurgerIngredient type='meat' />
-        <BurgerIngredient type='bacon' />
+        {igList}
         <BurgerIngredient type='bread-bottom' />
     </div >
 }
