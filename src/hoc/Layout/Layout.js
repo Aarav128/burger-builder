257b9c38def3e8fffdd431812f1a6c8a4ef3e670
Aarav Shah <aaravinaction@gmail.com>
2020-04-15 13:57:55 -0700

    restructured Layout into hoc

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 
 import classes from './Layout.css'
 
-import Aux from '../../hoc/Aux'
+import Aux from '../Aux/Aux'
 
-import Toolbar from '../Navigation/Toolbar/Toolbar'
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
 
 class Layout extends Component
 {
